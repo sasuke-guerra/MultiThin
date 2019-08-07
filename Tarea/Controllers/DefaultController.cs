9ccd693a1b83a160952cba02f675d7787fef094a
Katuishi <diogenes_mj@hotmail.com>
2019-08-07 12:33:15 -0400

    Lindo

---
+++ Tarea/Controllers/DefaultController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tarea.Models;
+namespace Tarea.Controllers
+{
+    public class DefaultController : Controller
+    {
+        List<Tareas> lastTarras;
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcesarFormulario(Tareas tareas)
+        {
+            if (ModelState.IsValid)
+            {
+                Algo.LTareas.Add(new Tareas()
+                {
+
+                    Descripcion = Request["des"],
+                    Fecha = DateTime.Parse(Request["fecha"]),
+                    Materia = Request["materia"]
+                });
+                return RedirectToAction("RegistroTarea");
+            }
+            else
+            {
+                return View("RegistroTarea");
+            }
+        }
+
+
+        public ActionResult RegistroTarea()
+        {
+
+
+            return View(Algo.LTareas);
+        }
+
+   
+        public ActionResult Calculadora()
+        {
+            return View();
+        }
+
+        public ActionResult Acerca()
+        {
+            return View();
+        }
+
+        public ActionResult Conversor()
+        {
+            return View();
+        }
+
+        public ActionResult Lector()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Tarea/Models/Tarea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tarea.Models
+{
+    static class Algo
+    {
+
+       public static List<Tareas> LTareas = new List<Tareas>();
+    }
+
+    public class Tareas
+    {
+        public string Materia { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
